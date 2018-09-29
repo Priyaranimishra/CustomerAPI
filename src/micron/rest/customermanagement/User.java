24ea6e565dd7e8383fb9e7735ad04f78aeb02fdf
Priyaranimishra <priyarani1294@gmail.com>
2018-09-29 14:02:51 +0530

    Update User.java

--- src/micron/rest/customermanagement/User.java
+++ src/micron/rest/customermanagement/User.java
@@ -12,7 +12,7 @@ public class User implements Serializable {
    private String name;
    private String profession;
 
-   public User(){}
+   public user(){}
    
    public User(int id, String name, String profession){
       this.id = id;
